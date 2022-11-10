2bfce3bcc869c4f62940891316e4bf7be1e4cdaa
Lucas Prado <116663065+Pradones@users.noreply.github.com>
2022-11-09 23:55:46 -0300

    Add files via upload
    Criar um programa que conte de 1 a 100, nos números múltiplos de 3, você deve imprimir a palavra "Pin" invés do número, nos múltiplos de 5 imprima "Pan", e nos números de 3 e 5 imprima "Pin pan", o restante dos números serão impressos normalmente.

---
+++ desafioparte2.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	for x := 1; x <= 100; x++ {
+
+		if x%3 == 0 && x%5 == 0 {
+			fmt.Println("Pin Pan")
+		} else if x%3 == 0 {
+			fmt.Println("Pin")
+		} else if x%5 == 0 {
+			fmt.Println("Pan")
+		} else {
+			fmt.Println(x)
+		}
+	}
+}
